eaafa5c4eb12c3a04576a0918015450236b4ce55
Josorteg <114577940+Josorteg@users.noreply.github.com>
2024-10-01 14:17:04 +0000

    ejemplo

---
+++ Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync("Bienvenido a la API. Usa /api/planets para obtener información de los planetas.");
+});
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    foreach (var forecas in forecast)
+        System.Console.WriteLine(forecas);
+    return forecast;
+});
+
+app.MapGet("/fuck",() =>
+{
+    return("fuck you more");
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapGet("/files/{fileName}", (string fileName) =>
+{
+    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "files", fileName);
+    System.Console.WriteLine(filePath);
+    if (!System.IO.File.Exists(filePath))
+    {
+        System.Console.WriteLine("ERROR : ",filePath);
+        return Results.NotFound(); // Retorna 404 si el archivo no existe
+    }
+
+    var contentType = "application/octet-stream"; // Cambiar según el tipo de archivo
+    return Results.File(filePath, contentType, fileName); // Devuelve el archivo
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
--- README.es
+++ README.es
@@ -1,1 +1,9 @@
 Prueba para el servidor de la hackaton Nasa app chanllenge
+sudo apt-get update
+
+sudo apt-get install -y wget
+
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+dotnet new webapi -o NASA
